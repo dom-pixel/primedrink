3316750421c66bbf99302167b6b75efa113f04e3
Gabriel Augusto Almeida <almeidagabrielaugusto@gmail.com>
2020-04-08 14:20:47 -0400

    models com os fillables

--- app/Plan.php
+++ app/Plan.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Plan extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'percentage',
+        'status'
+    ];
 }
--- app/Restaurant.php
+++ app/Restaurant.php
@@ -6,5 +6,32 @@ use Illuminate\Database\Eloquent\Model;
 
 class Restaurant extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'cnpj',
+        'name',
+        'phone',
+        'zipcode',
+        'street',
+        'number',
+        'complement',
+        'neighborhood',
+        'state',
+        'city',
+        'delivery',
+        'plan_id',
+        'full_name',
+        'document',
+        'document_secondary',
+        'document_secondary_complement',
+        'bank_id',
+        'agency',
+        'account',
+        'digit',
+        'is_linked'
+    ];
 }
