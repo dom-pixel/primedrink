c32cc9eda8c84aed7b1746c3382ae5efb3b42ca1
Gabriel Augusto Almeida <almeidagabrielaugusto@gmail.com>
2020-04-13 18:16:37 -0400

    teste

--- app/User.php
+++ app/User.php
@@ -45,6 +45,6 @@ class User extends Authenticatable
 
     public function restaurant()
     {
-        return $this->hasOne('App\Restaurant');
+        return $this->hasOne('App\Restaurant', 'restaurant_id', 'id');
     }
 }
