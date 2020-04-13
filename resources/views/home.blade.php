e8031767517e32e4c9912806a56ab1e8d4d30477
Gabriel Augusto Almeida <almeidagabrielaugusto@gmail.com>
2020-04-13 18:31:06 -0400

    teste

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-lg-12">
-                <div class="card">
+                <div class="card col-12">
                     <div class="card-header">Dashboard</div>
 
                     <div class="card-body">
