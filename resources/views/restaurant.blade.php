c8d1559aed54296446153363505a8186af34fa8a
Gabriel Augusto Almeida <almeidagabrielaugusto@gmail.com>
2020-04-14 09:37:56 -0400

    finalizando

--- resources/views/restaurant.blade.php
+++ resources/views/restaurant.blade.php
@@ -5,10 +5,13 @@
         <div class="row justify-content-center">
             <div class="col-lg-12">
                 <div class="card col-12">
-                    <div class="card-header">Informações do {{$restaurant->trending_name}}</div>
-
+                    <div class="card-header">Informações do {{$restaurant->trading_name}}</div>
                     <div class="card-body">
-
+                        <p class="sub-header">Formulário de edição de dados cadastrais. </p>
+                        <b><p>Razão Social:</b> {{$restaurant->company_name}}</p>
+                        <b><p>Fantasia:</b> {{$restaurant->trading_name}}</p>
+                        <b><p>CNPJ:</b> {{$restaurant->cnpj}}</p>
+                        <b><p>Telefone:</b> {{$restaurant->phone}}</p>
                     </div>
                 </div>
             </div>
