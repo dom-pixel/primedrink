d9ea080374a1ff16ea2c57f63bb17526655afccb
Gabriel Augusto Almeida <almeidagabrielaugusto@gmail.com>
2020-04-14 09:57:51 -0400

    teste

--- resources/views/restaurant.blade.php
+++ resources/views/restaurant.blade.php
@@ -8,19 +8,19 @@
                     <div class="card-header">Informações da distribuidora/empório '{{$restaurant->trading_name}}'</div>
                     <div class="card-body">
                         <b><p class="text-dark">Informações da Distribuidora/Empório:</p></b>
-                        <p>Razão Social:{{$restaurant->company_name}}</p>
-                        <p>Fantasia:{{$restaurant->trading_name}}</p>
-                        <p>CNPJ:{{$restaurant->cnpj}}</p>
-                        <p>Telefone:{{$restaurant->phone}}</p>
-                        <p>Endereço:{{$restaurant->street}}, Nº{{$restaurant->number}},
+                        <p>Razão Social: {{$restaurant->company_name}}</p>
+                        <p>Fantasia: {{$restaurant->trading_name}}</p>
+                        <p>CNPJ: {{$restaurant->cnpj}}</p>
+                        <p>Telefone: {{$restaurant->phone}}</p>
+                        <p>Endereço: {{$restaurant->street}}, Nº{{$restaurant->number}},
                             Bairro {{$restaurant->neighborhood}}, {{$restaurant->complement}}, {{$restaurant->city}}
                             /{{$restaurant->state}}, {{$restaurant->zipcode}}</p>
-                        <p>Especialidade:{{$restaurant->business}}</p>
-                        <p>Possui delivery:{{($restaurant->delivery === 1 ? 'Sim' : 'Não')}}</p>
+                        <p>Especialidade: {{$restaurant->business}}</p>
+                        <p>Possui delivery: {{($restaurant->delivery === 1 ? 'Sim' : 'Não')}}</p>
                         <b><p class="text-dark">Informações do Dono:</p></b>
-                        <p>Nome Completo:{{$restaurant->full_name}}</p>
-                        <p>CPF:{{$restaurant->document}}</p>
-                        <p>RG:{{$restaurant->document_secondary}}
+                        <p>Nome Completo: {{$restaurant->full_name}}</p>
+                        <p>CPF: {{$restaurant->document}}</p>
+                        <p>RG: {{$restaurant->document_secondary}}
                             / {{$restaurant->document_secondary_complement}}</p>
                     </div>
                 </div>
