d47d60a866f6f86431323d8104d8a3668e7a7b43
Gabriel Augusto Almeida <almeidagabrielaugusto@gmail.com>
2020-04-14 09:31:52 -0400

    restaurante

--- resources/views/restaurant.blade.php
+++ resources/views/restaurant.blade.php
@@ -5,39 +5,10 @@
         <div class="row justify-content-center">
             <div class="col-lg-12">
                 <div class="card col-12">
-                    <div class="card-header">Dashboard</div>
+                    <div class="card-header">Informações do {{$restaurant->trending_name}}</div>
 
                     <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
-                        <table class="table table-responsive-lg table-responsive">
-                            <thead>
-                            <tr>
-                                <th>#</th>
-                                <th>Nome</th>
-                                <th>Estabelecimento</th>
-                                <th>E-mail</th>
-                                <th>Celular</th>
-                                <th>Cidade/Estado
-                                <th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            @foreach($users as $user)
-                                <tr>
-                                    <td>{{$user->id}}</td>
-                                    <td>{{$user->name}}</td>
-                                    <td>{{$user->restaurant->trading_name}}</td>
-                                    <td>{{$user->email}}</td>
-                                    <td class="celular">{{$user->cell}}</td>
-                                    <td>{{$user->restaurant->city}}/ {{$user->restaurant->state}}</td>
-                                </tr>
-                            @endforeach
-                            </tbody>
-                        </table>
+
                     </div>
                 </div>
             </div>
