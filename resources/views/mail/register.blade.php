01d7392a9b409fe671b8afe0fdb41d18451f5131
Gabriel Augusto Almeida <almeidagabrielaugusto@gmail.com>
2020-04-16 17:45:57 -0400

    email refatorado

--- resources/views/mail/register.blade.php
+++ resources/views/mail/register.blade.php
@@ -1,6 +1,9 @@
 @component('mail::message')
     <h1>Pré-inscrição feita com sucesso!</h1>
-    <p> Olá, {{ $name }}. Estamos muito felizes em seu interesse no Prime Drink!</p>
-    <p>Agora é só aguardar um de nossos especialistas entra em contato com você!</p>
+    <p> Olá, {{ $name }}. tudo prime? 😉</p>
+    <p>Estamos felizes em seu interesse em se credenciar na Prime Drink.
+        Recebemos todas as suas informações com sucesso! 🍻
+    </p>
+    <p>Agora é só aguardar um de nossos especialistas entrar em contato com você e finalizar seu credenciamento! </p>
     *E-mail enviado automático através do sistema.
 @endcomponent
