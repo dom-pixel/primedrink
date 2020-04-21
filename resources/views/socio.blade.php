7fd3a3e9b1e1075d59cb67ed4e70b9a7351c5cd1
Gabriel Augusto Almeida <almeidagabrielaugusto@gmail.com>
2020-04-21 15:13:53 -0400

    teste em prod

--- resources/views/socio.blade.php
+++ resources/views/socio.blade.php
@@ -10,66 +10,23 @@
                     <form action="{{ route('sendRegisterSubscription') }}" method="POST" data-toggle="validator"
                           data-focus="false">
                         @csrf
-                        <div class="content-form-home">
-                            <!-- Mensagem de sucesso no cadastro e envio de e-mail -->
-                            @if ($message = Session::get('success'))
-                                <div class="alert alert-info alert-block">
-                                    <button type="button" class="close" data-dismiss="alert">×</button>
-                                    <strong>{{ $message }}</strong>
-                                </div>
-                            @endif
-                        <!-- Mensagem de erro do envio de e-mail -->
-                            @if ($message = Session::get('error'))
-                                <div class="alert alert-danger alert-block">
-                                    <button type="button" class="close" data-dismiss="alert">×</button>
-                                    <strong>{{ $message }}</strong>
-                                </div>
-                            @endif
-                        <!-- Mensagem de erro do Subscription Request -->
-                            @if($errors->all())
-                                @foreach($errors->all() as $error)
-                                    <div class="alert alert-danger alert-block">
-                                        <button type="button" class="close" data-dismiss="alert">×</button>
-                                        <strong>{{ $error }}</strong>
-                                    </div>
-                                @endforeach
-                            @endif
-                            <div>
-                                <span class="text-form-01">Cadastre sua distribuidora e decole!</span>
-                            </div>
-                            <div class="m-t-10">
-                                <span class="text-form-02">Pensou em aumentar a rentabilidade do seu negócio?</span>
-                            </div>
-                            <div>
-                                <span class="text-form-02">Pensou Prime Drink!</span>
-                            </div>
+                        <div class="content-form-socio">
+                            <span class="text-form-socio-01">Seja um Sócio Operador!</span>
                             <div class="m-t-40">
-                                <span class="text-form-03">Nome Completo</span>
-                                <div>
-                                    <input class="form-control" type="text" name="name"
-                                           placeholder="Qual o nome completo do dono da distribuidora?" required>
-                                </div>
+                                <input class="form-control" type="text" name="name"
+                                       placeholder="Nome" required>
                             </div>
                             <div class="m-t-20">
-                                <span class="text-form-03">E-mail</span>
-                                <div>
-                                    <input class="form-control" type="email" name="email"
-                                           placeholder="Qual o e-mail do dono da distribuidora?" required>
-                                </div>
+                                <input class="form-control" type="email" name="email"
+                                       placeholder="Sobrenome" required>
                             </div>
                             <div class="m-t-20">
-                                <span class="text-form-03">Celular + DDD</span>
-                                <div>
-                                    <input class="form-control celular" type="text" name="cell"
-                                           placeholder="Qual o Celular com DDD do dono da distribuidora?" required>
-                                </div>
-                            </div>
-                            <div class="m-t-30">
-                                <span class="text-form-04">Ao continuar, aceito que a Prime Drink entre em contato comigo por telefone, whatsApp ou E-mail (Incluindo mensagens automáticas para credenciamento e ajuda do credenciado).</span>
+                                <input class="form-control celular" type="text" name="cell"
+                                       placeholder="E-mail" required>
                             </div>
                             <div class="m-t-10">
-                                <button class="btn-lg btn-success btn-block" type="submit">
-                                    Iniciar o meu cadastro!
+                                <button class="btn-lg btn-yellow btn-block" type="submit">
+                                   QUERO SER UM FRANQUEADO
                                 </button>
                             </div>
                         </div>
