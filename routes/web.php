a16966cb75811acf44191a84a72e3371970eff32
Gabriel Augusto Almeida <almeidagabrielaugusto@gmail.com>
2020-04-29 15:44:31 -0400

    teste

--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,8 @@ Route::post('/send/email', 'Web\LandingPageController@mail')->name('sendRegister
 Route::get('/login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('/login', 'Auth\LoginController@login');
 Route::get('/logout', 'Auth\LoginController@logout')->name('logout');
-//Route::get('/register', 'Auth\RegisterController@showRegistrationForm')->name('register');
-//Route::post('/register', 'Auth\RegisterController@register');
+Route::get('/register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+Route::post('/register', 'Auth\RegisterController@register');
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/restaurante/{id}', 'HomeController@show')->name('info');
