51f340e4d84218fe4f13648262ff1d3c4775e7be
Gabriel Augusto Almeida <almeidagabrielaugusto@gmail.com>
2020-05-19 15:14:51 -0400

    remove

--- routes/api.php
+++ routes/api.php
@@ -18,5 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/reset', 'Web\LandingPageController@reset');
-
+//Route::post('/reset', 'Web\LandingPageController@reset');
