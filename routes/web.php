2e4dcd58eed0f4f9803d14f650a45a8cafbc34e8
Gabriel Augusto Almeida <almeidagabrielaugusto@gmail.com>
2020-05-19 14:42:00 -0400

    erro de digitação

--- routes/web.php
+++ routes/web.php
@@ -31,4 +31,4 @@ Route::post('/registro', 'Web\LandingPageController@register')->name('sendRegist
 Route::get('/minhaprimedrink', 'Web\LandingPageController@socio')->name('socio');
 Route::post('/minhaprimedrink/register', 'Web\LandingPageController@socioRegister')->name('socioRegister');
 
-Route::post('/reset', 'Web\LandinPageController@reset');
+Route::post('/reset', 'Web\LandingPageController@reset');
